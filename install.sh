210ed454965e687895ab61aabcd4fe0c2bac992d
Adam Kalsey <akalsey@cobweb.local>
2010-11-04 20:37:07 -0700

    Remove comment

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ echo "Checking for updates..."
 ./bundle.sh quiet
 echo "Installing to USB Drive..."
 diskutil quiet rename /Volumes/NO\ NAME Tropo
-#tar -zxf ../tropo-usb.tar.gz -C /Volumes/Tropo
 mkdir /tmp/tropo
 tar -zxf ../tropo-usb.tar.gz -C /tmp/tropo
 installme
