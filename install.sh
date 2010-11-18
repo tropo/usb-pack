d6b864361fac3d66db2713ff21c489af36527f3f
Adam Kalsey <akalsey@cobweb.local>
2010-11-17 19:16:47 -0800

    (Fixed) After installing one drive, the second one doesn't get renamed (Fixed) Subsequent installs don't show the progress message

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 ## Bundle set up the USB drive and copy contents
 
 function installme {
+  echo "Installing to USB Drive..."
+  diskutil quiet rename /Volumes/NO\ NAME Tropo
   cp -R /tmp/tropo/* /Volumes/Tropo
   diskutil eject /Volumes/Tropo
   read -p "Do you have another drive inserted you would like to install to? (y/n):"
@@ -10,8 +12,6 @@ function installme {
 
 echo "Checking for updates..."
 ./bundle.sh quiet
-echo "Installing to USB Drive..."
-diskutil quiet rename /Volumes/NO\ NAME Tropo
 mkdir /tmp/tropo
 tar -zxf ../tropo-usb.tar.gz -C /tmp/tropo
 installme
